c4a9fba3259662f85c03e3e2298056d877d62843
osksun <osksun-6@student.ltu.se>
2020-04-20 20:32:15 +0200

    add bimg and bx to solution

--- Sne/premake5.lua
+++ Sne/premake5.lua
@@ -1,8 +1,19 @@
 
-local BGFX_DIR = "bgfx"
-local BIMG_DIR = "bimg"
-local BX_DIR = "bx"
-local GLFW_DIR = "glfw"
+local BGFX_DIR = "extlibs/bgfx"
+local BIMG_DIR = "extlibs/bimg"
+local BX_DIR = "extlibs/bx"
+local GLFW_DIR = "extlibs/glfw"
+
+function setBxCompat()
+	filter "action:vs*"
+		includedirs { path.join(BX_DIR, "include/compat/msvc") }
+	filter { "system:windows", "action:gmake" }
+		includedirs { path.join(BX_DIR, "include/compat/mingw") }
+	filter { "system:macosx" }
+		includedirs { path.join(BX_DIR, "include/compat/osx") }
+		buildoptions { "-x objective-c++" }
+end
+
 
 workspace "Sne"
 	architecture "x64"
@@ -87,7 +98,7 @@ project "Sandbox"
 
 		filter "configurations:Release"
 			defines "SNE_RELEASE"
-<<<<<<< HEAD
+
 			symbols "On"
 
 
@@ -95,6 +106,55 @@ project "Sandbox"
 
 		--filters {"system:windows", "configurations:Release"}
 		--buildoptions "/MT"
-=======
+
 			symbols "on"
->>>>>>> 1f1d8f62a97d781d77b28734aeb7e16e8167a454
+
+project "bimg"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++14"
+	exceptionhandling "Off"
+	rtti "Off"
+	files
+	{
+		path.join(BIMG_DIR, "include/bimg/*.h"),
+		path.join(BIMG_DIR, "src/image.cpp"),
+		path.join(BIMG_DIR, "src/image_gnf.cpp"),
+		path.join(BIMG_DIR, "src/*.h"),
+		path.join(BIMG_DIR, "3rdparty/astc-codec/src/decoder/*.cc")
+	}
+	includedirs
+	{
+		path.join(BX_DIR, "include"),
+		path.join(BIMG_DIR, "include"),
+		path.join(BIMG_DIR, "3rdparty/astc-codec"),
+		path.join(BIMG_DIR, "3rdparty/astc-codec/include"),
+	}
+	setBxCompat()
+
+project "bx"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++14"
+	exceptionhandling "Off"
+	rtti "Off"
+	defines "__STDC_FORMAT_MACROS"
+	files
+	{
+		path.join(BX_DIR, "include/bx/*.h"),
+		path.join(BX_DIR, "include/bx/inline/*.inl"),
+		path.join(BX_DIR, "src/*.cpp")
+	}
+	excludes
+	{
+		path.join(BX_DIR, "src/amalgamated.cpp"),
+		path.join(BX_DIR, "src/crtnone.cpp")
+	}
+	includedirs
+	{
+		path.join(BX_DIR, "3rdparty"),
+		path.join(BX_DIR, "include")
+	}
+	filter "action:vs*"
+		defines "_CRT_SECURE_NO_WARNINGS"
+	setBxCompat()
