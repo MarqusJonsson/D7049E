ca3378ee89c8a147248cdb8b993ff4b3b7c2d9a2
Markus Jonsson <markusjonsson1997@gmail.com>
2020-04-22 11:37:12 +0200

    Add glfw support

--- Sne/premake5.lua
+++ Sne/premake5.lua
@@ -1,20 +1,3 @@
-
-local BGFX_DIR = "extlibs/bgfx"
-local BIMG_DIR = "extlibs/bimg"
-local BX_DIR = "extlibs/bx"
-local GLFW_DIR = "extlibs/glfw"
-
-function setBxCompat()
-	filter "action:vs*"
-		includedirs { path.join(BX_DIR, "include/compat/msvc") }
-	filter { "system:windows", "action:gmake" }
-		includedirs { path.join(BX_DIR, "include/compat/mingw") }
-	filter { "system:macosx" }
-		includedirs { path.join(BX_DIR, "include/compat/osx") }
-		buildoptions { "-x objective-c++" }
-end
-
-
 workspace "Sne"
 	architecture "x64"
 	startproject "Sandbox"
@@ -26,7 +9,14 @@ workspace "Sne"
 	}
 	
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
-	
+
+-- Include directories relative to root folder (solution directory)
+IncludeDir = {}
+IncludeDir["glfw"] = "Sne/extlibs/glfw/include"
+
+group "Dependencies"
+	include "Sne/extlibs/glfw"
+
 project "Sne"
 	location "Sne"
 	kind "SharedLib"
@@ -42,16 +32,27 @@ project "Sne"
 		"%{prj.name}/src/**.h",
 		"%{prj.name}/src/**.cpp"
 	}
-
-	includedirs { "extlibs/glfw/include/GLFW", "extlibs/bx/include/bx", "extlibs/bimg/include/bimg"}
 	
+	defines 
+	{
+		"_CRT_SECURE_NO_WARNINGS",
+		"GLFW_INCLUDE_NONE"
+	}
+	
+	includedirs
+	{
+		"%{prj.name/src",
+		"%{IncludeDir.glfw}"
+	}
+	
+	links
+	{
+		"glfw",
+		"opengl32.lib"
+	}
+
 	filter "system:windows"
 		systemversion "latest"
-		
-		defines
-		{
-			"SNE_BUILD_DLL",
-		}
 
 		postbuildcommands
 		{
@@ -84,172 +85,24 @@ project "Sandbox"
 
 	includedirs
 	{
-		"Sne/src"
+		"Sne/src",
+		"Sne/extlibs"
 	}
+	
+	links
+	{
+		"Sne"
+	}
+	
 	filter "system:windows"
 		systemversion "latest"
-		
-		links
-		{
-			"Sne"
-		}
 
 		filter "configurations:Debug"
 			defines "SNE_DEBUG"
+			runtime "Debug"
 			symbols "on"
 
 		filter "configurations:Release"
 			defines "SNE_RELEASE"
-
+			runtime "Release"
 			symbols "On"
-
-
-
-
-		--filters {"system:windows", "configurations:Release"}
-		--buildoptions "/MT"
-
-			symbols "on"
-
-project "bimg"
-	kind "StaticLib"
-	language "C++"
-	cppdialect "C++14"
-	exceptionhandling "Off"
-	rtti "Off"
-	files
-	{
-		path.join(BIMG_DIR, "include/bimg/*.h"),
-		path.join(BIMG_DIR, "src/image.cpp"),
-		path.join(BIMG_DIR, "src/image_gnf.cpp"),
-		path.join(BIMG_DIR, "src/*.h"),
-		path.join(BIMG_DIR, "3rdparty/astc-codec/src/decoder/*.cc")
-	}
-	includedirs
-	{
-		path.join(BX_DIR, "include"),
-		path.join(BIMG_DIR, "include"),
-		path.join(BIMG_DIR, "3rdparty/astc-codec"),
-		path.join(BIMG_DIR, "3rdparty/astc-codec/include"),
-	}
-	setBxCompat()
-
-project "bx"
-	kind "StaticLib"
-	language "C++"
-	cppdialect "C++14"
-	exceptionhandling "Off"
-	rtti "Off"
-	defines "__STDC_FORMAT_MACROS"
-	files
-	{
-		path.join(BX_DIR, "include/bx/*.h"),
-		path.join(BX_DIR, "include/bx/inline/*.inl"),
-		path.join(BX_DIR, "src/*.cpp")
-	}
-	excludes
-	{
-		path.join(BX_DIR, "src/amalgamated.cpp"),
-		path.join(BX_DIR, "src/crtnone.cpp")
-	}
-	includedirs
-	{
-		path.join(BX_DIR, "3rdparty"),
-		path.join(BX_DIR, "include")
-	}
-	filter "action:vs*"
-		defines "_CRT_SECURE_NO_WARNINGS"
-	setBxCompat()
-
-project "bgfx"
-	kind "StaticLib"
-	language "C++"
-	cppdialect "C++14"
-	exceptionhandling "Off"
-	rtti "Off"
-	defines "__STDC_FORMAT_MACROS"
-	files
-	{
-		path.join(BGFX_DIR, "include/bgfx/**.h"),
-		path.join(BGFX_DIR, "src/*.cpp"),
-		path.join(BGFX_DIR, "src/*.h"),
-	}
-	excludes
-	{
-		path.join(BGFX_DIR, "src/amalgamated.cpp"),
-	}
-	includedirs
-	{
-		path.join(BX_DIR, "include"),
-		path.join(BIMG_DIR, "include"),
-		path.join(BGFX_DIR, "include"),
-		path.join(BGFX_DIR, "3rdparty"),
-		path.join(BGFX_DIR, "3rdparty/dxsdk/include"),
-		path.join(BGFX_DIR, "3rdparty/khronos")
-	}
-	filter "configurations:Debug"
-		defines "BGFX_CONFIG_DEBUG=1"
-	filter "action:vs*"
-		defines "_CRT_SECURE_NO_WARNINGS"
-		excludes
-		{
-			path.join(BGFX_DIR, "src/glcontext_glx.cpp"),
-			path.join(BGFX_DIR, "src/glcontext_egl.cpp")
-		}
-	filter "system:macosx"
-		files
-		{
-			path.join(BGFX_DIR, "src/*.mm"),
-		}
-	setBxCompat()
-
-	project "glfw"
-	kind "StaticLib"
-	language "C"
-	files
-	{
-		path.join(GLFW_DIR, "include/GLFW/*.h"),
-		path.join(GLFW_DIR, "src/context.c"),
-		path.join(GLFW_DIR, "src/egl_context.*"),
-		path.join(GLFW_DIR, "src/init.c"),
-		path.join(GLFW_DIR, "src/input.c"),
-		path.join(GLFW_DIR, "src/internal.h"),
-		path.join(GLFW_DIR, "src/monitor.c"),
-		path.join(GLFW_DIR, "src/osmesa_context.*"),
-		path.join(GLFW_DIR, "src/vulkan.c"),
-		path.join(GLFW_DIR, "src/window.c"),
-	}
-	includedirs { path.join(GLFW_DIR, "include") }
-	filter "system:windows"
-		defines "_GLFW_WIN32"
-		files
-		{
-			path.join(GLFW_DIR, "src/win32_*.*"),
-			path.join(GLFW_DIR, "src/wgl_context.*")
-		}
-	filter "system:linux"
-		defines "_GLFW_X11"
-		files
-		{
-			path.join(GLFW_DIR, "src/glx_context.*"),
-			path.join(GLFW_DIR, "src/linux*.*"),
-			path.join(GLFW_DIR, "src/posix*.*"),
-			path.join(GLFW_DIR, "src/x11*.*"),
-			path.join(GLFW_DIR, "src/xkb*.*")
-		}
-	filter "system:macosx"
-		defines "_GLFW_COCOA"
-		files
-		{
-			path.join(GLFW_DIR, "src/cocoa_*.*"),
-			path.join(GLFW_DIR, "src/posix_thread.h"),
-			path.join(GLFW_DIR, "src/nsgl_context.h"),
-			path.join(GLFW_DIR, "src/egl_context.h"),
-			path.join(GLFW_DIR, "src/osmesa_context.h"),
-
-			path.join(GLFW_DIR, "src/posix_thread.c"),
-			path.join(GLFW_DIR, "src/nsgl_context.m"),
-			path.join(GLFW_DIR, "src/egl_context.c"),
-			path.join(GLFW_DIR, "src/nsgl_context.m"),
-			path.join(GLFW_DIR, "src/osmesa_context.c"),                       
-		}
