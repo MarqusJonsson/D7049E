9174732134d307227db6047d9890a32d31d0dafe
osksun <osksun-6@student.ltu.se>
2020-05-28 12:50:18 +0200

    remove duplicate EventSystem folder and separated ComponentArray and IComponentArray

--- Sne/Sne/EventSystem/Event.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class Event
-{
-public:
-    virtual~Event();
-
-    virtual const char* type() const = 0;
-};
---
+++ Sne/Sne/src/Sne/ECS/ComponentArray.h
@@ -0,0 +1,71 @@
+#pragma once
+#include "ComponentArray.h"
+template<typename T>
+class ComponentArray : public IComponentArray
+{
+public:
+	void InsertData(Entity entity, T component)
+	{
+		assert(entityToIndexMap.find(entity) == entityToIndexMap.end() && "Component added to same entity more than once.");
+
+		// Put new entry at end and update the maps
+		size_t newIndex = size;
+		entityToIndexMap[entity] = newIndex;
+		indexToEntityMap[newIndex] = entity;
+		componentArray[newIndex] = component;
+		++size;
+	}
+
+	void RemoveData(Entity entity)
+	{
+		assert(entityToIndexMap.find(entity) != entityToIndexMap.end() && "Removing non-existent component.");
+
+		// Copy element at end into deleted element's place to maintain density
+		size_t indexOfRemovedEntity = entityToIndexMap[entity];
+		size_t indexOfLastElement = size - 1;
+		componentArray[indexOfRemovedEntity] = componentArray[indexOfLastElement];
+
+		// Update map to point to moved spot
+		Entity entityOfLastElement = indexToEntityMap[indexOfLastElement];
+		entityToIndexMap[entityOfLastElement] = indexOfRemovedEntity;
+		indexToEntityMap[indexOfRemovedEntity] = entityOfLastElement;
+
+		entityToIndexMap.erase(entity);
+		indexToEntityMap.erase(indexOfLastElement);
+
+		--size;
+	}
+
+	// Return a reference to the entity's component
+	T& GetData(Entity entity)
+	{
+		assert(entityToIndexMap.find(entity) != entityToIndexMap.end() && "Retrieving non-existent component.");
+
+		return componentArray[entityToIndexMap[entity]];
+	}
+
+	void EntityDestroyed(Entity entity) override
+	{
+		if (entityToIndexMap.find(entity) != entityToIndexMap.end())
+		{
+			// Remove the entity's component if it existed
+			RemoveData(entity);
+		}
+	}
+
+private:
+	// The packed array of components (of generic type T),
+	// set to a specified maximum amount, matching the maximum number
+	// of entities allowed to exist simultaneously, so that each entity
+	// has a unique spot.
+	eastl::array<T, MAX_ENTITIES> componentArray;
+
+	// Map from an entity ID to an array index.
+	eastl::unordered_map<Entity, size_t> entityToIndexMap;
+
+	// Map from an array index to an entity ID.
+	eastl::unordered_map<size_t, Entity> indexToEntityMap;
+
+	// Total size of valid entries in the array.
+	size_t size;
+};
